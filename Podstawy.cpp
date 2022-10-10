3cbf11ccc529a1f290b172774c543cdf25fdab7a
DevForYou.pl <ziut3k@protonmail.com>
2022-10-10 21:52:36 +0200

    a

--- Podstawy.cpp
+++ Podstawy.cpp
@@ -1,13 +1,26 @@
 ﻿#include <iostream>
-
+//Opisac i przygotowac komentarze
 using namespace std;
 
-
-int main()
+//dekralcja fukncji o naziwe funkcja
+int funkcja()
 {
+	//deklaracja zmiennnej o nazwie zmienna_z_slowem 
+	//ktora przechowuje tekst Czesc jestem ala
 	string zmienna_z_slowem = "Czesc jestem ala";
+
+
 	string druga_zmienna_z_slowem = "Czesc jest janek";
 	string spacja = druga_zmienna_z_slowem + zmienna_z_slowem;
 	cout <<spacja;
+}
+int czesc() {
+	cout << "czesc";
+
+}
+int main() {
+	cout << "main";
+	funkcja();
+	czesc();
 }
 
