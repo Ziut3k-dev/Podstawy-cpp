4e69c32387d427cda2c464692b46001110a83a42
DevForYou.pl <ziut3k@protonmail.com>
2022-10-10 21:22:41 +0200

    a

--- Podstawy.cpp
+++ Podstawy.cpp
@@ -3,15 +3,11 @@
 using namespace std;
 
 
-
 int main()
 {
-    int a = 2.1;
-    string b = "khajgb5sdhjab";
-    string liczba = "5";
-    cout << "Hello World!\n";
-    cout << a << endl;
-    cout << b << endl;
-    cout << liczba << endl;
+	string zmienna_z_slowem = "Czesc jestem ala";
+	string druga_zmienna_z_slowem = "Czesc jest janek";
+	string spacja = druga_zmienna_z_slowem + zmienna_z_slowem;
+	cout <<spacja;
 }
 
