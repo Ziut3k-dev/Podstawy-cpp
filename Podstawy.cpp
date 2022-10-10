16e76fde789969bccf011139d5bc66b088db532f
DevForYou.pl <ziut3k@protonmail.com>
2022-10-10 20:47:01 +0200

    Dodaj pliki projektów.

---
+++ Podstawy.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    int a = 2.1;
+    string b = "khajgb5sdhjab";
+    string liczba = "5";
+    cout << "Hello World!\n";
+    cout << a << endl;
+    cout << b << endl;
+    cout << liczba << endl;
+}
+
